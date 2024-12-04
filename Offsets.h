18c4fb937900c5874c055a3ae8a1f3ecd4739d35
SofMain <vladirus1vlad@gmail.com>
2024-12-04 17:45:54 +0000

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -5,24 +5,16 @@ PLAYER_CONTROLLER 0x30 //fresh dump
 LOCAL_PAWN 0x350 //fresh dump
 PAWN_PRIVATE 0x320 //fresh dump
 ROOT_COMPONENT 0x1B0 //fresh dump
-RELATIVE_LOCATION 0x138 //fresh dump
+RELATIVE_LOCATION 0x120 //fresh dump
 PLAYER_STATE 0x2C8 //fresh dump
-TEAM_INDEX 0x1261 //fresh dump
-GAME_STATE 0x178 //fresh dump
+TEAM_INDEX 0x1261 fresh dump
+GAME_STATE 0x178 fresh dump
 PLAYER_ARRAY 0x2C0 //fresh dump
 MESH 0x328 //fresh dump
 COMPONENT_TO_WORLD 0x1E0 //fresh dump
 BONE_ARRAY 0x5A8 //fresh dump
 BONE_ARRAY_CACHE 0x590 //fresh dump
-LAST_SUBMIT_TIME 0x2F0 //fresh dump
-LAST_SUBMIT_TIME_ON_SCREEN 0X2E8 //fresh dump
+LAST_SUBMIT_TIME 0x2E8 //fresh dump
+LAST_SUBMIT_TIME_ON_SCREEN 0x2F0 //fresh dump
 VELOCITY 0x168 //fresh dump
-PersistentLevel = 0x40 //fresh dump
-WeaponData = 0x568 //fresh dump
-CurrentWeapons = 0xa80 //fresh dump
-PlayerName: 0xAF8 //fresh dump
-ProjectileSpeed: 0x1A24 //fresh dump
-ProjectileGravity: 0x1DF0 //fresh dump
-
-
-  ALL OFFSETS UPDATED ENJOY!
+ALL OFFSETS UPDATED ENJOY!
