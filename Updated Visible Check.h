16ac8330140e6e104119c8ac61ec65edc1434671
SofMain <vladirus1vlad@gmail.com>
2025-08-08 15:58:55 +0300

    Update Updated Visible Check.h

--- Updated Visible Check.h
+++ Updated Visible Check.h
@@ -1,5 +1,6 @@
 bool is_visible(uintptr_t mesh, float tolerance = 0.06f) {
-	double Seconds = read<double>(cache::uworld + 0x160);
+	double Seconds = read<double>(cache::uworld + 0x190);
 	float LastRenderTime = read<float>(mesh + 0x32C);
 	return Seconds - LastRenderTime <= tolerance;
 }
+
